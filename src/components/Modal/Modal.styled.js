380aef9c2d1ea4f733bbe1b8d10e92fa5fd758f3
Bohdan9924 <bohdanminiailo22@gmail.com>
2023-08-19 18:12:59 +0200

    complete

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -19,7 +19,7 @@ export const ModalContent = styled.div`
     padding:10px;
 
     border-radius: 5px;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 10px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
 
     background-color:#fff;
     `
